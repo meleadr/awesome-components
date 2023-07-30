a7e398c51ff131bc237e7e3b23a0163e50f0e677
meleadr <meledina.adriano@gmail.com>
2023-07-30 15:12:14 +0200

    Header avec mat-toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import {SharedModule} from "../shared/shared.module";
+import { RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import {SharedModule} from "../shared/shared.module";
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
