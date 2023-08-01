4926ef023de4635b4c6866ee0d2bb9fd8d009b26
meleadr <meledina.adriano@gmail.com>
2023-08-01 15:29:20 +0200

    Animation avec Angular

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,32 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Comment} from "../../../core/models/comment.model";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.1)',
+        'background-color': 'rgb(201,157,242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-out')
+      ])
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit{
 
@@ -13,6 +34,7 @@ export class CommentsComponent implements OnInit{
   @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
+  listItemAnimationState: 'default' | 'active' = 'default';
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -25,5 +47,13 @@ export class CommentsComponent implements OnInit{
     if(this.commentCtrl.invalid) return;
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+  }
+
+  onListItemMouseEnter() {
+    this.listItemAnimationState = 'active';
+  }
+
+  onListItemMouseLeave() {
+    this.listItemAnimationState = 'default';
   }
 }
