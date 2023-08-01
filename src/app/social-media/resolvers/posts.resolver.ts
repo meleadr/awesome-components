59be77263b304884ed559da79fade45678f4dda3
meleadr <meledina.adriano@gmail.com>
2023-08-01 13:07:12 +0200

    Mise en place du resolver post

---
+++ src/app/social-media/resolvers/posts.resolver.ts
@@ -0,0 +1,16 @@
+import {Post} from "../models/post.model";
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {Injectable} from "@angular/core";
+import {PostService} from "../services/post.service";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class PostsResolver implements Resolve<Post[]>{
+  constructor(private postService: PostService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Post[]> {
+    return this.postService.getPosts();
+  }
+
+}
--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialMediaRoutingModule } from './social-media-routing.module';
 import {PostService} from "./services/post.service";
+import {PostsResolver} from "./resolvers/posts.resolver";
+import { PostListComponent } from './components/post-list/post-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostListComponent
+  ],
   imports: [
     CommonModule,
     SocialMediaRoutingModule
   ],
   providers: [
-    PostService
+    PostService,
+    PostsResolver
   ]
 })
 export class SocialMediaModule { }
