f542f4888416d0b7a7dac5f58370f38cd2df4c3a
meleadr <meledina.adriano@gmail.com>
2023-08-01 13:39:42 +0200

    Creation de comments et material module

---
+++ src/app/shared/components/comments/comments.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {Comment} from "../../../core/models/comment.model";
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent {
+
+  @Input() comments!: Comment[];
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatCardModule} from "@angular/material/card";
+import { CommentsComponent } from './components/comments/comments.component';
+import {MaterialModule} from "./material.module";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CommentsComponent
+  ],
   imports: [
     CommonModule,
+    MaterialModule
   ],
   exports: [
-    MatToolbarModule,
-    MatCardModule
+    CommentsComponent,
+    MaterialModule
   ]
 })
 export class SharedModule { }
