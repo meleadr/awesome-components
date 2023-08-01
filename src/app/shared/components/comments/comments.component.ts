701c4d3ac05040334049a16f690c22475c21c2aa
meleadr <meledina.adriano@gmail.com>
2023-08-01 13:59:58 +0200

    EventEmitter ajout d'un comment

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Comment} from "../../../core/models/comment.model";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
 
@@ -10,6 +10,7 @@ import {FormBuilder, FormControl, Validators} from "@angular/forms";
 export class CommentsComponent implements OnInit{
 
   @Input() comments!: Comment[];
+  @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
 
@@ -21,6 +22,8 @@ export class CommentsComponent implements OnInit{
   }
 
   onLeaveComment() {
-
+    if(this.commentCtrl.invalid) return;
+    this.newComment.emit(this.commentCtrl.value);
+    this.commentCtrl.reset();
   }
 }
