a1668856d09ce2cebd6655e70412ae83bb67c237
meleadr <meledina.adriano@gmail.com>
2023-08-02 14:23:32 +0200

    Complex form implémentation

--- src/app/complex-form/components/complex-form/complex-form.component.ts
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-complex-form',
@@ -9,16 +9,55 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class ComplexFormComponent implements OnInit{
 
   mainForm!: FormGroup;
+  personalInfoForm!: FormGroup;
+  contactPreferenceCtrl!: FormControl;
+  emailCtrl!: FormControl;
+  confirmEmailCtrl!: FormControl;
+  emailForm!: FormGroup;
+  phoneCtrl!: FormControl;
+  passwordCtrl!: FormControl;
+  confirmPasswordCtrl!: FormControl;
+  loginInfoForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.initFormControls();
     this.initMainForm();
   }
 
-  initMainForm():void {
-    this.mainForm = this.formBuilder.group({});
+  private initMainForm():void {
+    this.mainForm = this.formBuilder.group({
+      personalInfo: this.personalInfoForm,
+      contactPreference: this.contactPreferenceCtrl,
+      email: this.emailForm,
+      phone: this.phoneCtrl,
+      loginInfo: this.loginInfoForm
+    });
+  }
+
+  private initFormControls() {
+    this.personalInfoForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+
+    this.contactPreferenceCtrl = this.formBuilder.control('email', Validators.required);
+    this.emailCtrl = this.formBuilder.control('', [Validators.required, Validators.email]);
+    this.confirmEmailCtrl = this.formBuilder.control('', [Validators.required, Validators.email]);
+    this.emailForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      confirmEmail: this.confirmEmailCtrl
+    });
+    this.phoneCtrl = this.formBuilder.control('');
+    this.passwordCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(8)]);
+    this.confirmPasswordCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(8)]);
+    this.loginInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl
+    });
   }
 
   onSubmitForm() {
